4321b124fbe0e393b1b7eace11948faf30d87a3d
Burak KOSE <burakks41@gmail.com>
2017-04-28 09:59:57 +0200

    Add operation types

---
+++ src/main/scala/com/github/gate4s/operation/OperationType.scala
@@ -0,0 +1,15 @@
+package com.github.gate4s.operation
+
+sealed trait OperationType
+
+object OperationType {
+
+  case object Pre extends OperationType
+
+  case object Routing extends OperationType
+
+  case object Post extends OperationType
+
+  case object Error extends OperationType
+
+}
